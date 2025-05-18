65060e40e76be06271ae138d2a5c21915336ac0c
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2025-05-18 23:58:08 +0200

    add endpoint to serve chat application TypeScript file

--- tests/functional/entrypoints/test_index.py
+++ tests/functional/entrypoints/test_index.py
@@ -8,3 +8,10 @@ async def test_index(test_client: AsyncClient) -> None:
 
     assert response.status_code == HTTPStatus.OK
     assert response.headers["content-type"] == "text/html; charset=utf-8"
+
+
+async def test_main_ts(test_client: AsyncClient) -> None:
+    response = await test_client.get("/chat_app.ts")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers["content-type"] == "text/plain; charset=utf-8"
