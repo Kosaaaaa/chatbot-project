266a4b7d9c89854dcae464196ee76dcb7274bfd6
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2025-05-18 22:36:46 +0200

    initialize project structure with configuration and dependency management

---
+++ app/config.py
@@ -0,0 +1,7 @@
+"""This module contains the configuration settings for the application."""
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """This class contains the configuration settings for the application."""
---
+++ app/main.py
@@ -0,0 +1,3 @@
+from app.dependencies import get_settings
+
+_SETTINGS = get_settings()
