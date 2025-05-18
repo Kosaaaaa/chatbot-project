db25932c89f70c56bab112b7c32998e230dc6028
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2025-05-18 23:48:32 +0200

    add project directory path configuration for improved settings management

--- app/config.py
+++ app/config.py
@@ -1,9 +1,12 @@
 """This module contains the configuration settings for the application."""
 
+from pathlib import Path
 from typing import Literal
 
 from pydantic import Field
 from pydantic_settings import BaseSettings
+
+PROJECT_DIR = Path(__file__).parent
 
 
 class Settings(BaseSettings):
