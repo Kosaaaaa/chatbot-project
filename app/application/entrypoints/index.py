b83c4b1c671adf2a1aaeddcf8e6aef02d1895f64
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2025-05-18 23:58:08 +0200

    add endpoint to serve chat application TypeScript file

--- app/application/entrypoints/index.py
+++ app/application/entrypoints/index.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from fastapi.responses import FileResponse
 
-from app.application.entrypoints.dependencies.static import get_index_html_file_path
+from app.application.entrypoints.dependencies.static import get_chat_app_ts_file_path, get_index_html_file_path
 
 index_router = APIRouter()
 
@@ -12,3 +12,8 @@ index_router = APIRouter()
 @index_router.get("/")
 async def index(index_html: Annotated[Path, Depends(get_index_html_file_path)]) -> FileResponse:
     return FileResponse(index_html, media_type="text/html")
+
+
+@index_router.get("/chat_app.ts")
+async def main_ts(chat_app_ts: Annotated[Path, Depends(get_chat_app_ts_file_path)]) -> FileResponse:
+    return FileResponse(chat_app_ts, media_type="text/plain")
