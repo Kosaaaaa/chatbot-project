607af71053e98c0bd320201de0a9a277b6ffec12
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2025-05-18 22:43:48 +0200

    add entry point for FastAPI application with Uvicorn server

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
+from app.application.entrypoints.api import create_app
 from app.dependencies import get_settings
 
 _SETTINGS = get_settings()
+
+app = create_app(settings=_SETTINGS)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_config=None, log_level=None)
