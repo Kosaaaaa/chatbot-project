0d636bc99f0473dc239b4e2e67f68f425eba968d
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2025-05-19 21:01:49 +0200

    add endpoint to retrieve chat messages

--- app/application/entrypoints/chat.py
+++ app/application/entrypoints/chat.py
@@ -1,3 +1,20 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.responses import Response
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlmodel import select
+
+from app.application.entrypoints.dependencies.storage import get_session
+from app.domain.models.chat_message import ChatMessage
 
 chat_router = APIRouter(prefix="/chat", tags=["chat"])
+
+
+@chat_router.get("/")
+async def get_chat(session: Annotated[AsyncSession, Depends(get_session)]):
+    result = await session.execute(select(ChatMessage))
+    return Response(
+        b"\n".join([m.model_dump_json().encode() for m in result.scalars().all()]),
+        media_type="text/plain",
+    )
